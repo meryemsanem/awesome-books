6a96b03db686b36abc7b39a2d29da3deeda7e6ea
Henok Kirubel <henokhm2@gmail.com>
2023-06-05 15:56:53 +0300

    Add JS function to load books into HTML page

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const form = document.getElementById("form");
-form.addEventListener("submit", (e) => {
+const awesomeBooks = [];
+
+const booksList = document.querySelector('#books');
+const form = document.getElementById('form');
+
+const loadBooks = () => {
+  awesomeBooks.forEach((book) => {
+    const li = document.createElement('li');
+    const title = document.createElement('span');
+    const author = document.createElement('span');
+    const removeBtn = document.createElement('button');
+
+    title.classList.add("title");
+    author.classList.add("author");
+
+    title.innerText = book.title;
+    author.innerText = book.author;
+    removeBtn.innerText = "Remove";
+
+    li.appendChild(title);
+    li.appendChild(author);
+    li.appendChild(removeBtn);
+    booksList.appendChild(li);
+  });
+};
   e.preventDefault();
   console.log(e.target);
 });
